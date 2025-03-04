6e5109b6e131b068b3e6d58ea45b8f7948e4a2ea
juansito17 <69127937+juansito17@users.noreply.github.com>
2025-03-04 01:45:31 -0500

    Añadí catalogo de peliculas

--- js/logica.js
+++ js/logica.js
@@ -1,3 +1,9 @@
 function login() {
     window.location.href = "/html/login.html";
 }
+
+function cambiarPelicula(nombre, duraccion) {
+    document.getElementById("titulo").innerHTML = nombre;
+    document.getElementById("duraccion").innerHTML = duraccion;
+    document.getElementById("imagen").src = "/images/" + nombre + ".jpg";
+}
